f3a1aee1ed42ee3f533c7e794689aef3cd131a7e
priyanshusrivastava049-afk <priyanshusrivastava049@gmail.con>
2026-07-28 09:02:01 +0530

    day2

--- Day2.js
+++ Day2.js
@@ -6,12 +6,31 @@
 // hello();
 // console.log("task 2");
 
-function hello(){
-    console.log("hello");
-    setTimeout(function(){
-        console.log("task2");
-    },2000
-)
+// function hello(){
+//     console.log("hello");
+//     setTimeout(function(){
+//         console.log("task2");
+//     },2000
+// )
+// }
+// hello();
+// console.log("task 3");
+function hello(n1,n2){
+    console.log("task 1");
+    return n1+n2;
 }
-hello();
-console.log("task 3");
+let a=10;
+let b=20;
+console.log(hello(a,b));
+function hi(){
+    console.log("say hi");
+}
+hello(a,b,hi());
+function demo(){
+    console.log("Demo");
+}
+function Text(){
+console.log("Text");
+}
+hello(a,b,Text());
+hello(a,b,demo());
