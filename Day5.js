cf2e423f155c77bb4129c980bb61305707e25a62
priyanshusrivastava049-afk <priyanshusrivastava049@gmail.con>
2026-08-18 08:58:24 +0530

    day 5

--- Day5.js
+++ Day5.js
@@ -1,14 +1,30 @@
-const EventEmitter=require("events");
-class MyEvent extends EventEmitter{}
-event = new MyEvent();
-event.on("greet" , (name)=>{
-    console.log(`Hello, ${name}!`);
-});
-const events=new EventEmitter();
-events.once("greet",()=>{
-    console.log("this is event emitter");
-})
-event.on("exit",()=>{})
-events.emit("greet");
-events.emit("exit");
+//Program 1 
+//emit() on()
+// const EventEmitter=require("events");
+// const event=new EventEmitter();
+// // event.on("greet",()=>{
+// //     console.log("this is event emitter");
     
+// // })
+// event.once("greet",()=>{
+//     console.log("event trigger only one time");
+    
+// })
+// event.emit("greet");
+// event.emit("greet");
+// event.emit("greet");
+// event.emit("greet");
+//Program 1:create custom EventEmitter that trigger "greet" or "exit"
+class MyEmitter extends EventEmitter{}
+const event=new MyEmitter()
+event.on("greet",(name)=>{
+console.log(`hello ${name}`);//template literals`${}`
+
+})
+event.on("exit",()=>{
+    console.log("exits mycustom event emitter..");
+    
+})
+event.emit("greet","cse24");
+event.emit("exit");
+Full stack development workshop program 1
