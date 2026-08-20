0634827d76766c0674d1a34b8a4537d15e9863d8
priyanshusrivastava049-afk <priyanshusrivastava049@gmail.con>
2026-08-20 11:32:01 +0530

    Day7

--- Day6/server.js
+++ Day6/server.js
@@ -4,6 +4,17 @@ const server=http.createServer((req,res)=>{
 res.write("welcome to my server");
  res.end();
 })
+
+server.on("error", (error) => {
+    if (error.code === "EADDRINUSE") {
+        console.error("Port 8000 is already in use. Stop the other server or use a different port.");
+        return;
+    }
+
+    throw error;
+});
+
 server.listen(8000,()=>{
     console.log("server is running on port 8000");
 })
+cl
